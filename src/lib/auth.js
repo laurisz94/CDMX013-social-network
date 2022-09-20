257e66cb62d4fa73d54b236e66c746402a1c36ca
laurisz94 <lauriisz1994@gmail.com>
2022-09-20 13:48:02 -0500

    Fix main.js and adding buttons github & google

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,12 @@
 /* eslint-disable max-len */
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { app } from './config.js';
 
 const auth = getAuth();
 
 export const adduserWithEmail = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 // return implícito
+
+const providerGoogle = new GoogleAuthProvider();
+const providerTwitter = new TwitterAuthProvider();
+const providerGithub = new GithubAuthProvider();
