6e57d74e4ec164e0667aff0c8633e3e42f91b71c
laurisz94 <lauriisz1994@gmail.com>
2022-09-20 19:17:34 -0500

    finish layout 2 and add providers

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
+import {
+ getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider, signInWithRedirect, getRedirectResult } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { app } from './config.js';
 
 const auth = getAuth();
@@ -7,6 +8,10 @@ const auth = getAuth();
 export const adduserWithEmail = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 // return implícito
 
-const providerGoogle = new GoogleAuthProvider();
-const providerTwitter = new TwitterAuthProvider();
-const providerGithub = new GithubAuthProvider();
+export const providerGoogle = new GoogleAuthProvider();
+export const providerTwitter = new TwitterAuthProvider();
+export const providerGithub = new GithubAuthProvider();
+
+export const signInUserWithProviders = (provider) => signInWithRedirect(auth, provider);
+
+export const redirectResult = () => getRedirectResult(auth);
